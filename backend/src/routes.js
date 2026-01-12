8d8fdcc50f80c81ef6f72b53176ef017f0eb3566
Pedro <pedrohgs2004@gmail.com>
2026-01-12 09:21:08 -0300

    FEAT: adicionando novas rotas

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import user from "./controllers/userController.js";
+import ator from "./controllers/actorController.js";
 import diretor from "./controllers/diretorcontroller.js";
+import genero from "./controllers/generoController.js";
 import login from "./controllers/loginController.js";
 import actor from "./controllers/actorController.js";
 import movie from "./controllers/movieController.js"
@@ -8,9 +10,13 @@ import movie from "./controllers/movieController.js"
 const routes = express();
 
 routes.use('/user', user);
+routes.use('/ator', ator);
 routes.use('/diretor', diretor);
+routes.use('/genero', genero);
 routes.use('/login', login);
 routes.use('/actor', actor);
 routes.use('/movie', movie);
 
 export default routes;
+
+
